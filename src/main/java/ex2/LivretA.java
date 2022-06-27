1d03bcf8c7c9fc2fec4a09564e1cda6cb30e8768
Philippe Maximin <philippe_maximin@yahoo.fr>
2022-06-27 15:16:10 +0200

    exo2

--- src/main/java/ex2/LivretA.java
+++ src/main/java/ex2/LivretA.java
@@ -1,9 +1,51 @@
 package ex2;
 
 public class LivretA extends CompteBancaire {
+	
+	/** tauxRemuneration : taux de rémunération dans le cas d'un livret A */
+	private double tauxRemuneration;
 
-	public LivretA(String type, double solde, double tauxRemuneration) {
-		super(type, solde, 0, tauxRemuneration);
+	
+	/**
+	 * Constructeur
+	 * @param solde
+	 * @param decouvert
+	 * @param tauxRemuneration
+	 */
+	public LivretA(double solde, double decouvert, double tauxRemuneration) {
+		super(solde, decouvert);
+		setTauxRemuneration(tauxRemuneration);
+	}
+	
+	
+	public void appliquerRemuAnnuelle(){
+		setSolde( getSolde()+(getSolde()*tauxRemuneration/100) );
+	}
+	
+	
+	/** Ajoute un montant au solde
+	 * @param montant
+	 */
+	public void debiterMontant(double montant){
+		if (getSolde() - montant > 0) {
+			setSolde( getSolde() - montant);
+		}	
+	}
+	
+	
+	/** Getter for tauxRemuneration
+	 * @return the tauxRemuneration
+	 */
+	public double getTauxRemuneration() {
+		return tauxRemuneration;
+	}
+	
+	
+	/** Setter
+	 * @param tauxRemuneration the tauxRemuneration to set
+	 */
+	public void setTauxRemuneration(double tauxRemuneration) {
+		this.tauxRemuneration = tauxRemuneration;
 	}
 
 }
